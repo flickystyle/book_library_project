d055c8114519edc747532dd05711a157928ee1e3
Dmitry <superflick3@gmail.com>
2024-08-24 12:56:07 +0300

    Add BookForm.jsx

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,9 +1,51 @@
+import { useState } from 'react';
 import './BookForm.css';
 
 const BookForm = () => {
+  const [bookInfo, setBookInfo] = useState({ title: '', author: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (bookInfo.title && bookInfo.author) {
+      //dispatch
+      console.log('dispatch');
+      setBookInfo({ title: '', author: '' });
+    }
+  };
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setBookInfo(
+      name === 'title'
+        ? { ...bookInfo, title: value }
+        : { ...bookInfo, author: value }
+    );
+  };
+
   return (
     <div className="app-block book-form">
-      <h2>BookForm</h2>
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            name="title"
+            value={bookInfo.title}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            name="author"
+            value={bookInfo.author}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <button type="submit">Add book</button>
+      </form>
     </div>
   );
 };
